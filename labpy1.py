67054945c1ee620790ea76ec1ba1549ea66a1a83
risva palintya <45660844+Risvapalintya@users.noreply.github.com>
2019-02-13 18:38:40 +0700

    Add files via upload

---
+++ labpy1.py
@@ -0,0 +1,9 @@
+a=int(input("Masukan Bilangan Ke -1 ="))
+b=int(input("Masukan Bilangan Ke- 2="))
+c=int(input("Masukan Bilangan ke -3="))
+if a>b and a>c:
+    print("Bilangan Terbesar Adalah =",a)
+elif b>a and b>c:
+    print("Bilangan Terbesar Adalah =",b)
+else4:
+    print("Bilangan Terbesar Adalah =",c)
